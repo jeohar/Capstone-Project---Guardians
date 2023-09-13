242e30284107ee756659ef0806ce6b6164083f08
jeohar <jeohar@outlook.com>
2023-09-13 09:12:40 -0700

    new changes on fireFox browsers class

--- src/main/java/tek/sdet/framework/config/FireFoxBrowser.java
+++ src/main/java/tek/sdet/framework/config/FireFoxBrowser.java
@@ -2,16 +2,21 @@ package tek.sdet.framework.config;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+import org.openqa.selenium.firefox.FirefoxOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FireFoxBrowser implements Browser{
-		@Override
+		//@Override
 		public WebDriver openBrowser(String url) {
 		WebDriverManager.firefoxdriver().setup();
-		WebDriver driver = new FirefoxDriver();
-		driver.get(url);
-		return driver;
+		FirefoxOptions options = new FirefoxOptions();
+	//	WebDriver driver = new FirefoxDriver();
+		
+		options.setHeadless(true);
+	    WebDriver driver = new FirefoxDriver(options);
+	    driver.get(url);
+	    return driver;
+		
 	}
 
 }
